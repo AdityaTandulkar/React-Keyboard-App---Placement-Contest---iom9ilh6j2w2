9f01e152565c45ac9f439f836808a365d2f39029
AdityaTandulkar <adityatandulkar20@gmail.com>
2023-05-28 15:14:44 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,39 +4,18 @@ import React, { useState, useEffect } from "react";
 const keys = "abcdefghijklmnopqrstuvwxyz0123456789 ".split("");
 
 const App = () => {
-    const [typedVal, setTypedVal] = useState("");
-    const [quote, setQuote] = useState("");
-
-    useEffect(()=>{
-        if(typedVal === "forty two"){
-            fetch("https://api.quotable.io/random")
-                .then((response)=> response.json())
-                .then((data) => setQuote(data.content))
-                .catch((err)=>console.log(err));
-        }
-    },[typedVal])
 
   return (
-    <>
-        {
-            quote === ""
-            ?
-            <>
-                <div className="keyboard">
-                    <div className="preview">{typedVal}</div>
-                    <div>
-                        {keys.map((key) => (
-                        <button key={key} id={key === " " ? `key-space` : `key-${key}`} onClick={()=>setTypedVal(typedVal+key)}>
-                            {key === " " ? "Space" : key.toUpperCase()}
-                        </button>
-                        ))}
-                    </div>
-                </div>
-            </>
-            :
-            <div className="quote">{quote}</div>
-        }
-    </>
+    <div className="keyboard">
+      <div className="preview"></div>
+      <div>
+        {keys.map((key) => (
+          <button key={key} id={key === " " ? `key-space` : `key-${key}`}>
+            {key === " " ? "Space" : key.toUpperCase()}
+          </button>
+        ))}
+      </div>
+    </div>
   );
 };
 
