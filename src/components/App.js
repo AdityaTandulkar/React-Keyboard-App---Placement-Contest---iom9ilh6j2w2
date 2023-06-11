3ee639eb77bf851b33c0a983cec28db5497c4e8b
AdityaTandulkar <adityatandulkar20@gmail.com>
2023-06-11 19:29:51 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,44 +1,35 @@
 import "../styles/App.css";
 import React, { useState, useEffect } from "react";
-import axios from 'axios';
 
 const keys = "abcdefghijklmnopqrstuvwxyz0123456789 ".split("");
 
 const App = () => {
-  const [preview, setPreview] = useState('');
-  const [quote, setQuote] = useState('');
+  const [input,setInput] = useState("");
+  const [quote,setQuote] = useState("");
 
-  const handleClick = (keyValue) => {
-    const updatedPreview = preview + keyValue;
-    setPreview(updatedPreview);
-
-    if (updatedPreview === 'forty two') {
-      axios.get('https://api.quotable.io/random').then((response) => {
-        setQuote(response.data.content);
-      });
-    } else {
-      setQuote('');
+  useEffect(()=>{
+    if (input === "forty two") {
+      fetch("https://api.quotable.io/random")
+      .then(res=>res.json())
+      .then(data=>setQuote(data.content))
+    }else{
+      setQuote("");
     }
-  };
-  const renderKeys = () => {
-   
-    return keys.map((keyValue) => (
-      <button key={`key-${keyValue}`} id={`key-${keyValue === ' ' ? 'space' : keyValue}`} onClick={() => handleClick(keyValue)}>
-        {keyValue}
-      </button>
-    ));
-  };
+  },[input])
 
   return (
-    <div className="App">
-      <div className="preview">{preview}</div>
-      {preview === 'forty two' && <div className="quote">{quote}</div>}
-      <div className="keyboard">{renderKeys()}</div>
+    <div className="keyboard">
+      <div className="preview">{input}</div>
+      {!!quote && <div className="quote">{quote}</div>}
+      <div>
+        {keys.map((key) => (
+          <button key={key} id={key === " " ? `key-space` : `key-${key}`} onClick={()=>{setInput(prev=>prev + key)}}>
+            {key === " " ? "Space" : key.toUpperCase()}
+          </button>
+        ))}
+      </div>
     </div>
   );
-
 };
-
-
 
 export default App;
